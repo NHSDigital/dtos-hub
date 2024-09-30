ac56152d7b00ab7131009fe53cae16bebac051c1
Richard Kingston <richard.kingston@nordcloud.com>
2024-09-30 18:24:17 +0100

    Add Private DNS Zones

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,3 +2,20 @@
 output "vnets_hub" {
   value = module.vnets_hub
 }
+
+# Output the private DNS zone IDs so they can be used in private endpoint modules
+output "private_dns_zone_app_services" {
+  value = module.private_dns_zone_app_services
+}
+
+output "private_dns_zone_azure_sql" {
+  value = module.private_dns_zone_azure_sql
+}
+
+output "private_dns_zone_storage_blob" {
+  value = module.private_dns_zone_storage
+}
+
+output "private_dns_zone_storage_queue" {
+  value = module.private_dns_zone_storage
+}

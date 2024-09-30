5c79f77e6288c3a640bcb3266a1279f1ee54540b
Richard Kingston <richard.kingston@nordcloud.com>
2024-09-30 18:24:17 +0100

    Added further outputs for private endpoint use

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,3 +2,16 @@
 output "vnets_hub" {
   value = module.vnets_hub
 }
+
+# Output the DNS ZOne Ids as they are required by the private endpoint module
+output "azurerm_private_dns_zone_app_services_id" {
+  value = azurerm_private_dns_zone.private_dns_zone_app_services.id
+}
+
+output "azurerm_private_dns_zone_azure_sql_id" {
+  value = azurerm_private_dns_zone.private_dns_zone_azure_sql.id
+}
+
+output "azurerm_private_dns_zone_storage_id" {
+  value = azurerm_private_dns_zone.private_dns_zone_storage.id
+}

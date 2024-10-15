f5dfed17791feab5967d15b10241d80cdbb4a7a3
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-10-15 17:13:44 +0100

    Added Azure Virtual Desktop

--- infrastructure/config.tf
+++ infrastructure/config.tf
@@ -2,7 +2,7 @@ module "config" {
   for_each = var.regions
 
   # Source location updated to use the git:: prefix to avoid URL encoding issues - note // between the URL and the path is required
-  source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/shared-config?ref=25800a1f5bce5c7cf0fcfe7725026f5c8f1542fb"
+  source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/shared-config?ref=32df88c630d01cda9395a3433315bf47dc6ed122"
 
   location    = each.key
   application = var.application

8d865958167013ee9f5ac3dee88ba639e9b1b49c
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-10-18 20:34:32 +0100

    redeploy VMs

--- infrastructure/config.tf
+++ infrastructure/config.tf
@@ -2,7 +2,7 @@ module "config" {
   for_each = var.regions
 
   # Source location updated to use the git:: prefix to avoid URL encoding issues - note // between the URL and the path is required
-  source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/shared-config?ref=32df88c630d01cda9395a3433315bf47dc6ed122"
+  source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/shared-config?ref=892f57534a22ec9b17046856c0bd28cfa3c90389"
 
   location    = each.key
   application = var.application

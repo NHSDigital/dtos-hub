c38f48ca778c58108ba8f62a93c6dafb8421fdf3
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-10-25 02:12:17 +0100

    Added Application Gateway

--- infrastructure/application_gateway.tf
+++ infrastructure/application_gateway.tf
@@ -18,7 +18,7 @@ module "application-gateway-pip" {
   zones                = each.value.is_primary_region ? ["1", "2", "3"] : null
   sku                  = "Standard"
 
-  tags                 = var.tags
+  tags = var.tags
 }
 
 module "application-gateway" {
@@ -27,11 +27,12 @@ module "application-gateway" {
   # Source location updated to use the git:: prefix to avoid URL encoding issues - note // between the URL and the path is required
   source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/application-gateway?ref=f552c0ffe1f5991248266c1b37bca49084412df1"
 
-  name                    = module.config[each.key].names.application-gateway.name
-  location                = each.key
-  resource_group_name     = azurerm_resource_group.agw[each.key].name
-  gateway_subnet          = module.subnets_hub["${module.config[each.key].names.subnet}-app-gateway"]
-  public_ip_address_id    = module.application-gateway-pip[each.key].id
+  name                 = module.config[each.key].names.application-gateway.name
+  location             = each.key
+  resource_group_name  = azurerm_resource_group.agw[each.key].name
+  common_names         = module.config[each.key].names.application-gateway
+  gateway_subnet       = module.subnets_hub["${module.config[each.key].names.subnet}-app-gateway"]
+  public_ip_address_id = module.application-gateway-pip[each.key].id
 
   tags = var.tags
 }
--- infrastructure/config.tf
+++ infrastructure/config.tf
@@ -2,7 +2,7 @@ module "config" {
   for_each = var.regions
 
   # Source location updated to use the git:: prefix to avoid URL encoding issues - note // between the URL and the path is required
-  source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/shared-config?ref=f552c0ffe1f5991248266c1b37bca49084412df1"
+  source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/shared-config?ref=f693566bc9d67bd8d7d5a196fb1a244ff7478ee2"
 
   location    = each.key
   application = var.application

5f0fe9351c32d794b5fe27aed915a22aae5e5974
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-10-25 14:11:37 +0100

    Added Application Gateway (#22)

--- infrastructure/config.tf
+++ infrastructure/config.tf
@@ -2,7 +2,7 @@ module "config" {
   for_each = var.regions
 
   # Source location updated to use the git:: prefix to avoid URL encoding issues - note // between the URL and the path is required
-  source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/shared-config?ref=783b7d1e39b4f8e299fad5018396c7399742a0dc"
+  source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/shared-config?ref=a97430c23c686fc16fa5cf387794c394aab01f54"
 
   location    = each.key
   application = var.application

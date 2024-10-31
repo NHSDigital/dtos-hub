ad218dee55deae83025681cbc8c6ac20a9b4b176
josielsouzanordcloud <90701149+josielsouzanordcloud@users.noreply.github.com>
2024-10-31 16:27:13 +0000

    Deploy and Configure APIM in Development Hub (#29)
    Co-authored-by: Josiel Souza <josielbr@gmail.com>

---
+++ infrastructure/api-management.tf
@@ -0,0 +1,40 @@
+module "api-management" {
+  for_each = var.regions
+
+  # Source location updated to use the git:: prefix to avoid URL encoding issues - note // between the URL and the path is required
+  source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/api-management?ref=0151db0bc6eb554a8e4ced044be36cb1cb7ba7b3"
+
+  name                          = "${module.config[each.key].names.api-management}"
+  resource_group_name           = azurerm_resource_group.rg_hub[each.key].name
+  location                      = each.key
+  certificate_details           = []
+  gateway_disabled              = var.apim_config.gateway_disabled
+  public_ip_address_id          = module.apim-public-ip[each.key].id
+  publisher_email               = var.apim_config.publisher_email
+  publisher_name                = var.apim_config.publisher_name
+  sku_capacity                  = var.apim_config.sku_capacity
+  sku_name                      = var.apim_config.sku_name
+  virtual_network_type          = var.apim_config.virtual_network_type
+  virtual_network_configuration = [module.subnets_hub["${module.config[each.key].names.subnet}-api-mgmt"].id]
+  zones                         = var.apim_config.zones
+
+  tags = var.tags
+
+}
+
+module "apim-public-ip" {
+  for_each = var.regions
+
+  # Source location updated to use the git:: prefix to avoid URL encoding issues - note // between the URL and the path is required
+  source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/public-ip?ref=03f00be0347a4b086911abefb67dc3dc432d8eff"
+
+  name                = "${module.config[each.key].names.public-ip-address}-api-mgmt"
+  resource_group_name = azurerm_resource_group.rg_hub[each.key].name
+  location            = each.key
+  allocation_method   = var.apim_config.public_ip_allocation_method
+  domain_name_label   = "${module.config[each.key].names.api-management}"
+  sku                 = var.apim_config.public_ip_sku
+  zones               = each.value.is_primary_region ? ["1"] : null
+
+  tags = var.tags
+}

899297957286399a2d668fa904c55d9cb29939b8
fahadbaigNC <fahad.baig@nordcloud.com>
2024-11-20 22:29:02 +0000

    feat: DTOS 5205 creating log analytic workspace hub

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -124,7 +124,7 @@ variable "key_vault" {
 variable "law" {
   description = "Configuration of the Log Analytics Workspace"
   type = object({
-    name                       = optional(string, "cohman")
+    name                       = optional(string, "hub")
     law_sku = optional(string, "PerGB2018")
     retention_days             = optional(number, 30)
   })

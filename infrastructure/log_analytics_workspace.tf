38765e7c6b0febc831f02f3670cac1a22b6c7c10
fahadbaigNC <fahad.baig@nordcloud.com>
2024-11-21 10:28:31 +0000

    feat: DTOS 5205 creating log analytic workspace hub

--- infrastructure/log_analytics_workspace.tf
+++ infrastructure/log_analytics_workspace.tf
@@ -3,13 +3,13 @@ module "log_analytics_workspace_hub" {
 
   source = "../../dtos-devops-templates/infrastructure/modules/log-analytics-workspace"
 
-  name     = module.regions_config[each.key].names.log-analytics-workspace
+  name     = module.config[each.key].names.log-analytics-workspace
   location = each.key
 
   law_sku                                      = var.law.law_sku
   retention_days                                                  = var.law.retention_days
 
-  resource_group_name = azurerm_resource_group.audit[each.key].name
+  resource_group_name = azurerm_resource_group.rg_base[each.key].name
 
   tags = var.tags
 }

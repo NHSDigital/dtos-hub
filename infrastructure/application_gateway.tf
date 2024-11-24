00a61d25e292d02a37fe21e1316418bd269253ce
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-11-24 11:17:13 +0000

    wip

--- infrastructure/application_gateway.tf
+++ infrastructure/application_gateway.tf
@@ -1,124 +1,129 @@
 locals {
-  frontend_port = {
-    https = 443
-  }
+  appgw_config = {
+    for region in keys(var.regions) : {
 
-  frontend_ip_configuration = {
-    private = {
-      subnet_id                     = var.gateway_subnet.id
-      private_ip_address            = cidrhost(var.gateway_subnet.address_prefixes[0], 225)
-      private_ip_address_allocation = "Static"
-    }
-    public = {
-      public_ip_address_id = var.public_ip_address_id
-    }
-  }
+      frontend_port = {
+        https = 443
+      }
 
-  backend_address_pool = {
-    apim_gateway = {
-      fqdns = [module.apim-private-dns-a-records["${each.key}-gateway"].fqdn]
-    }
-    apim_portal = {
-      fqdns = [module.apim-private-dns-a-records["${each.key}-development"].fqdn]
-    }
-  }
+      frontend_ip_configuration = {
+        private = {
+          subnet_id                     = module.subnets_hub["${module.config[region].names.subnet}-app-gateway"].id
+          private_ip_address            = cidrhost(module.subnets_hub["${module.config[region].names.subnet}-app-gateway"].address_prefixes[0], 225)
+          private_ip_address_allocation = "Static"
+        }
+        public = {
+          public_ip_address_id = module.application-gateway-pip[region].id
+        }
+      }
 
-  probe = {
-    apim_gateway = {
-      interval                                  = 30
-      path                                      = "/status-0123456789abcdef"
-      pick_host_name_from_backend_http_settings = true
-      protocol                                  = "Https"
-      timeout                                   = 120
-      unhealthy_threshold                       = 8
-    }
-    apim_portal = {
-      interval                                  = 60
-      path                                      = "/signin"
-      pick_host_name_from_backend_http_settings = true
-      protocol                                  = "Https"
-      timeout                                   = 300
-      unhealthy_threshold                       = 8
-    }
-  }
+      backend_address_pool = {
+        apim_gateway = {
+          fqdns = [module.apim-private-dns-a-records["${region}-gateway"].fqdn]
+        }
+        apim_portal = {
+          fqdns = [module.apim-private-dns-a-records["${region}-development"].fqdn]
+        }
+      }
 
-  ssl_certificate = {
-    private = {
-      key_vault_secret_id = module.lets_encrypt_certificate.key_vault_certificates["wildcard_private-${each.key}"].versionless_secret_id
-    }
-    public = {
-      key_vault_secret_id = module.lets_encrypt_certificate.key_vault_certificates["wildcard-${each.key}"].versionless_secret_id
-    }
-  }
+      probe = {
+        apim_gateway = {
+          interval                                  = 30
+          path                                      = "/status-0123456789abcdef"
+          pick_host_name_from_backend_http_settings = true
+          protocol                                  = "Https"
+          timeout                                   = 120
+          unhealthy_threshold                       = 8
+        }
+        apim_portal = {
+          interval                                  = 60
+          path                                      = "/signin"
+          pick_host_name_from_backend_http_settings = true
+          protocol                                  = "Https"
+          timeout                                   = 300
+          unhealthy_threshold                       = 8
+        }
+      }
 
-  backend_http_settings = {
-    apim_gateway = {
-      cookieBasedAffinity                 = "Disabled"
-      pick_host_name_from_backend_address = true
-      port                                = 443
-      probe_key                           = "apim_gateway"
-      protocol                            = "Https"
-      request_timeout                     = 180
-    }
-    apim_portal = {
-      cookieBasedAffinity                 = "Disabled"
-      pick_host_name_from_backend_address = true
-      port                                = 443
-      probe_key                           = "apim_portal"
-      protocol                            = "Https"
-      request_timeout                     = 180
-    }
-  }
+      ssl_certificate = {
+        private = {
+          key_vault_secret_id = module.lets_encrypt_certificate.key_vault_certificates["wildcard_private-${region}"].versionless_secret_id
+        }
+        public = {
+          key_vault_secret_id = module.lets_encrypt_certificate.key_vault_certificates["wildcard-${region}"].versionless_secret_id
+        }
+      }
 
-  http_listener = {
-    apim_portal_private = {
-      frontend_ip_configuration_key = "private"
-      frontend_port_key             = "https"
-      hostname                      = "portal.${var.dns_zone_name_private}"
-      protocol                      = "Https"
-      require_sni                   = true
-      ssl_certificate_key           = "private"
-    }
-    apim_gateway_private = {
-      frontend_ip_configuration_key = "private"
-      frontend_port_key             = "https"
-      hostname                      = "api.${var.dns_zone_name_private}"
-      protocol                      = "Https"
-      require_sni                   = true
-      ssl_certificate_key           = "private"
-    }
-    apim_gateway_public = {
-      frontend_ip_configuration_key = "public"
-      frontend_port_key             = "https"
-      hostname                      = "api.${var.dns_zone_name_public}"
-      protocol                      = "Https"
-      require_sni                   = true
-      ssl_certificate_key           = "public"
-      # firewall_policy_id            =
-    }
-  }
+      backend_http_settings = {
+        apim_gateway = {
+          cookieBasedAffinity                 = "Disabled"
+          pick_host_name_from_backend_address = true
+          port                                = 443
+          probe_key                           = "apim_gateway"
+          protocol                            = "Https"
+          request_timeout                     = 180
+        }
+        apim_portal = {
+          cookieBasedAffinity                 = "Disabled"
+          pick_host_name_from_backend_address = true
+          port                                = 443
+          probe_key                           = "apim_portal"
+          protocol                            = "Https"
+          request_timeout                     = 180
+        }
+      }
 
-  request_routing_rule = {
-    apim_gateway_public = {
-      backend_address_pool_key  = "apim_gateway"
-      backend_http_settings_key = "apim_gateway"
-      http_listener_key         = "apim_gateway_public"
-      priority                  = 900
-      rule_type                 = "Basic"
-    }
-    apim_gateway_private = {
-      backend_address_pool_key  = "apim_gateway"
-      backend_http_settings_key = "apim_gateway"
-      http_listener_key         = "apim_gateway_private"
-      priority                  = 1000
-      rule_type                 = "Basic"
-    }
-    apim_portal_private = {
-      backend_address_pool_key  = "apim_portal"
-      backend_http_settings_key = "apim_portal"
-      http_listener_key         = "apim_portal_private"
-      priority                  = 1100
-      rule_type                 = "Basic"
+      http_listener = {
+        apim_portal_private = {
+          frontend_ip_configuration_key = "private"
+          frontend_port_key             = "https"
+          hostname                      = "portal.${var.dns_zone_name_private}"
+          protocol                      = "Https"
+          require_sni                   = true
+          ssl_certificate_key           = "private"
+        }
+        apim_gateway_private = {
+          frontend_ip_configuration_key = "private"
+          frontend_port_key             = "https"
+          hostname                      = "api.${var.dns_zone_name_private}"
+          protocol                      = "Https"
+          require_sni                   = true
+          ssl_certificate_key           = "private"
+        }
+        apim_gateway_public = {
+          frontend_ip_configuration_key = "public"
+          frontend_port_key             = "https"
+          hostname                      = "api.${var.dns_zone_name_public}"
+          protocol                      = "Https"
+          require_sni                   = true
+          ssl_certificate_key           = "public"
+          # firewall_policy_id            =
+        }
+      }
+
+      request_routing_rule = {
+        apim_gateway_public = {
+          backend_address_pool_key  = "apim_gateway"
+          backend_http_settings_key = "apim_gateway"
+          http_listener_key         = "apim_gateway_public"
+          priority                  = 900
+          rule_type                 = "Basic"
+        }
+        apim_gateway_private = {
+          backend_address_pool_key  = "apim_gateway"
+          backend_http_settings_key = "apim_gateway"
+          http_listener_key         = "apim_gateway_private"
+          priority                  = 1000
+          rule_type                 = "Basic"
+        }
+        apim_portal_private = {
+          backend_address_pool_key  = "apim_portal"
+          backend_http_settings_key = "apim_portal"
+          http_listener_key         = "apim_portal_private"
+          priority                  = 1100
+          rule_type                 = "Basic"
+        }
+      }
     }
   }
 }
@@ -149,19 +154,19 @@ module "application-gateway" {
   resource_group_name       = azurerm_resource_group.rg_hub[each.key].name
   autoscale_min             = 1
   autoscale_max             = 10
-  backend_address_pool      = local.backend_address_pool
-  backend_http_settings     = local.backend_http_settings
-  frontend_ip_configuration = local.frontend_ip_configuration
-  frontend_port             = local.frontend_port
-  http_listener             = local.http_listener
+  backend_address_pool      = local.appgw_config[each.key].backend_address_pool
+  backend_http_settings     = local.appgw_config[each.key].backend_http_settings
+  frontend_ip_configuration = local.appgw_config[each.key].frontend_ip_configuration
+  frontend_port             = local.appgw_config[each.key].frontend_port
+  http_listener             = local.appgw_config[each.key].http_listener
   key_vault_id              = module.key_vault[each.key].key_vault_id
   names                     = module.config[each.key].names.application-gateway
   gateway_subnet            = module.subnets_hub["${module.config[each.key].names.subnet}-app-gateway"]
-  probe                     = local.probe
+  probe                     = local.appgw_config[each.key].probe
   public_ip_address_id      = module.application-gateway-pip[each.key].id
-  request_routing_rule      = local.request_routing_rule
+  request_routing_rule      = local.appgw_config[each.key].request_routing_rule
   sku                       = "WAF_v2"
-  ssl_certificate           = local.ssl_certificate
+  ssl_certificate           = local.appgw_config[each.key].ssl_certificate
   zones                     = each.value.is_primary_region ? ["1", "2", "3"] : null
 
   tags = var.tags

62cac122271cee57f9f119eeb35e2c4231d38be3
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-11-24 22:59:11 +0000

    wip

--- infrastructure/dns_public.tf
+++ infrastructure/dns_public.tf
@@ -2,11 +2,17 @@
   Application Gateway Public DNS A Records
 --------------------------------------------------------------------------------------------------*/
 
+locals {
+  appgw_public_listener_hostnames = ["api"]
+}
+
 module "appgw-dns-a-records" {
+  for_each = toset(local.appgw_public_listener_hostnames)
+
   # No region loop since DNS is global. Traffic Manager will be required if an additional region is added.
   source = "../../dtos-devops-templates/infrastructure/modules/dns-a-record"
 
-  name                = "api"
+  name                = each.key
   resource_group_name = var.dns_zone_rg_name_public
   zone_name           = var.dns_zone_name_public
   ttl                 = 300

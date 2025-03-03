0d9e20bc9b187fe3e3645a3554fe15075f3d3d1a
fahadbaigNC <fahad.baig@nordcloud.com>
2025-03-03 15:33:17 +0000

    feat: DTOS 6031 subscription activity logs for nonlive

--- infrastructure/subscriptions.tf
+++ infrastructure/subscriptions.tf
@@ -18,10 +18,10 @@ data "azurerm_subscription" "subscriptions" {
 -------------------------------------------------------------------------------------------------- */
 
 module "diagnostic-settings" {
-  source                                                = "../../dtos-devops-templates/infrastructure/modules/diagnostic-settings"
-  for_each                                              = var.subscriptions
-  name                                                  = "${each.key}-diagnostic-setting"
-  target_resource_id                                    = data.azurerm_subscription.subscriptions[each.key].id
-  log_analytics_workspace_id                            = module.log_analytics_workspace_hub[local.primary_region].id
-  monitor_diagnostic_setting_subscriptions_enabled_logs = local.monitor_diagnostic_setting_subscriptions_enabled_logs
+  source                     = "../../dtos-devops-templates/infrastructure/modules/diagnostic-settings"
+  for_each                   = var.subscriptions
+  name                       = "${each.key}-diagnostic-setting"
+  target_resource_id         = data.azurerm_subscription.subscriptions[each.key].id
+  log_analytics_workspace_id = module.log_analytics_workspace_hub[local.primary_region].id
+  enabled_log                = local.monitor_diagnostic_setting_subscriptions_enabled_logs
 }

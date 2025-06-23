278316d3d3033606247a2092ec52e20d81cfb965
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2025-06-23 01:48:27 +0100

    wip

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -14,6 +14,10 @@ data "azuread_service_principal" "MicrosoftAzureAppService" {
   client_id = "abfa0a7c-a6b6-4736-8310-5855508787cd"
 }
 
+data "azuread_service_principal" "MicrosoftAzureFrontDoorCdn" {
+  client_id = "205478c0-bd83-4e1b-a9d6-db63a3e1e1c8"
+}
+
 data "azurerm_key_vault_secret" "object-id" {
   for_each     = var.regions
   name         = "dtos-apim-object-id"

d55027f82b4c6121c145dbaed0da7a99e6ea329c
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2025-06-26 16:22:09 +0100

    amendments from review

--- infrastructure/frontdoor.tf
+++ infrastructure/frontdoor.tf
@@ -15,10 +15,10 @@ module "frontdoor_profile" {
   metric_enabled                                    = var.diagnostic_settings.metric_enabled
 
   # Front Door Profile is a global resource, hence the use of primary_region Key Vault
+  certificate_secrets = { for k in each.value.frontdoor_profile.secrets : k => module.acme_certificate[k].key_vault_certificate[local.primary_region].versionless_id }
   name                = "${module.config[local.primary_region].names.front-door-profile}-${each.value.short_name}"
   resource_group_name = azurerm_resource_group.rg_project["${each.key}-${local.primary_region}"].name
   sku_name            = each.value.frontdoor_profile.sku_name
-  secrets             = { for k in each.value.frontdoor_profile.secrets : k => module.acme_certificate[k].key_vault_certificate[local.primary_region].versionless_id }
 
   identity = each.value.frontdoor_profile.identity
 

b67e6b147f911d0bee88a55240e045ec3e7966d8
Alastair Lock <alastair.lock@gmail.com>
2025-07-01 12:39:21 +0100

    DTOSS-9711: Create Azure Service Health Alerts

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -376,6 +376,41 @@ variable "regions" {
   }))
 }
 
+variable "monitor_action_group" {
+  description = "Default configuration for the monitor action groups."
+  type = map(object({
+    short_name = string
+    email_receiver = optional(map(object({
+      name                    = string
+      email_address           = string
+      use_common_alert_schema = optional(bool, false)
+    })))
+    event_hub_receiver = optional(map(object({
+      name                    = string
+      event_hub_namespace     = string
+      event_hub_name          = string
+      subscription_id         = string
+      use_common_alert_schema = optional(bool, false)
+    })))
+    sms_receiver = optional(map(object({
+      name         = string
+      country_code = string
+      phone_number = string
+    })))
+    voice_receiver = optional(map(object({
+      name         = string
+      country_code = string
+      phone_number = string
+    })))
+    webhook_receiver = optional(map(object({
+      name                    = string
+      service_uri             = string
+      use_common_alert_schema = optional(bool, false)
+    })))
+
+  }))
+}
+
 variable "storage_accounts" {
   description = "Configuration for the Storage Account, currently used for Function Apps"
   type = map(object({
